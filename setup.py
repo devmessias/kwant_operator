f9c0c4dc9edf817bc3972dadb58eca1fd2d70866
bruno_messias <messias.physics@gmail.com>
2017-05-08 13:20:30 -0300

    Initial files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'kwant_operator',
+    packages = ['kwant_operator'],
+    version = '0.1',  
+    description = 'An extension for kwant. Allows another calculations',
+    author = 'devmessias',
+    author_email = 'messias.physics@gmail.com',
+    url = 'https://github.com/devmessias/kwant_operator',
+    download_url = 'https://github.com/devmessias/kwant_operator/archive/0.1.tar.gz',
+    keywords = ['kwant', 'quantum'],
+    classifiers = [],
+)
